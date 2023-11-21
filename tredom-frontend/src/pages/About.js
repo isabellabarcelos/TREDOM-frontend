04df2e82e1347196bf0b1c90c70d8ebad6c38596
isabellabarcelos <isabellabarcelossg@gmail.com>
2023-11-20 22:13:50 -0300

    routes working

---
+++ tredom-frontend/src/pages/About.js
@@ -0,0 +1,12 @@
+import Header  from "../components/Header";
+
+
+export default function About() {
+    return (
+      <>
+        <Header/>
+        <h2>About page</h2>
+      </>
+    );
+  }
+  
